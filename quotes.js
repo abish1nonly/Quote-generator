5bf0d4581b3280368c74fd07efa2373c74837bf2
unknown <abisanthadchanamoorthy@yahoo.com>
2023-07-29 21:05:25 +0530

    initial commit

---
+++ quotes.js
@@ -0,0 +1,110 @@
+const myQuotes = [
+    {
+        "text": "Genius is one percent inspiration and ninety-nine percent perspiration.",
+        "author": "Thomas Edison"
+      },
+      {
+        "text": "You can observe a lot just by watching.",
+        "author": "Yogi Berra"
+      },
+      {
+        "text": "A house divided against itself cannot stand.",
+        "author": "Abraham Lincoln"
+      },
+      {
+        "text": "Difficulties increase the nearer we get to the goal.",
+        "author": "Johann Wolfgang von Goethe"
+      },
+      {
+        "text": "Fate is in your hands and no one elses",
+        "author": "Byron Pulsifer"
+      },
+      {
+        "text": "Be the chief but never the lord.",
+        "author": "Lao Tzu"
+      },
+      {
+        "text": "Nothing happens unless first we dream.",
+        "author": "Carl Sandburg"
+      },
+      {
+        "text": "Well begun is half done.",
+        "author": "Aristotle"
+      },
+      {
+        "text": "Life is a learning experience, only if you learn.",
+        "author": "Yogi Berra"
+      },
+      {
+        "text": "Self-complacency is fatal to progress.",
+        "author": "Margaret Sangster"
+      },
+      {
+        "text": "Peace comes from within. Do not seek it without.",
+        "author": "Buddha"
+      },
+      {
+        "text": "What you give is what you get.",
+        "author": "Byron Pulsifer"
+      },
+      {
+        "text": "We can only learn to love by loving.",
+        "author": "Iris Murdoch"
+      },
+      {
+        "text": "Life is change. Growth is optional. Choose wisely.",
+        "author": "Karen Clark"
+      },
+      {
+        "text": "You'll see it when you believe it.",
+        "author": "Wayne Dyer"
+      },
+      {
+        "text": "Today is the tomorrow we worried about yesterday.",
+        "author":  "Abish"
+      },
+      {
+        "text": "When you have a dream, you've got to grab it and never let go.",
+        "author": "Carol Burnett"
+      },
+      {
+        "text": "Nothing is impossible. The word itself says 'I'm possible!",
+        "author":  "Audrey Hepburn"
+      },
+      {
+        "text": "There is nothing impossible to they who will try.",
+        "author":  "Alexander the Great"
+      },
+      {
+        "text": "The bad news is time flies. The good news is you're the pilot.",
+        "author":  "Michael Altshuler"
+      },
+      {
+        "text": "Life has got all those twists and turns. You've got to hold on tight and off you go.",
+        "author":  "Nicole Kidman"
+      },
+      {
+        "text": "Keep your face always toward the sunshine, and shadows will fall behind you.",
+        "author":  "Walt Whitman"
+      },
+      {
+        "text": "Be courageous. Challenge orthodoxy. Stand up for what you believe in. When you are in your rocking chair talking to your grandchildren many years from now, be sure you have a good story to tell.",
+        "author":  "Amal Clooney"
+      },
+      {
+        "text": "You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box.",
+        "author":  "Duchess Meghan"
+      },
+      {
+        "text": "I just want you to know that if you are out there and you are being really hard on yourself right now for something that has happened ... it's normal. That is what is going to happen to you in life. No one gets through unscathed. We are all going to have a few scratches on us. Please be kind to yourselves and stand up for yourself, please.",
+        "author":  "Taylor Swift"
+      },
+      {
+        "text": "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+        "author":  "Winston Churchill"
+      },
+      {
+        "text": "You define your own life. Don't let other people write your script.",
+        "author":  "Oprah Winfrey"
+      }
+]
---
+++ script.js
@@ -0,0 +1,71 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const newQuoteBtn = document.getElementById('new-quote');
+const twitterBtn = document.getElementById('twitter');
+const whatsappBtn = document.getElementById('whatsapp');
+const loader = document.getElementById('loader');
+
+
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete(){
+    if(!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+
+async function getQuote() {
+    loading();
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // If Author is blank, add 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        // Stop Loader, Show Quote
+        complete();
+    } catch (error) {
+        getQuote();
+    }
+}
+
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.
+    textContent} - ${authorText.textContent}`;
+    const url = encodeURIComponent(window.location.href); 
+    const via = "abish1nonly"
+
+    window.open(`${twitterUrl} link: ${url} by @${via}`, '_blank'); 
+}
+
+function whatsappSend(){
+    const whatsappUrl = `whatsapp://send?text=${quoteText.
+    textContent} - ${authorText.textContent}`;
+    const via = "abish1nonly"
+
+    window.open(whatsappUrl, '_blank'); 
+}
+
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+whatsappBtn.addEventListener('click', whatsappSend);
+
+getQuote();
